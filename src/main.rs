462df9139e02053591e244d653d4baccd8230c74
Arthur Eubanks <ayueubanks@gmail.com>
2020-08-29 23:43:32 -0700

    Use structopt to read in files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,24 @@ mod generator;
 mod style;
 
 use generator::{Generator, GeneratorParameters};
+use std::path::PathBuf;
+use structopt::StructOpt;
 use style::Style;
 
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Opts {
+    #[structopt(parse(from_os_str))]
+    inputs: Vec<PathBuf>,
+}
+
 fn main() {
+    let opts = Opts::from_args();
+
+    for p in opts.inputs {
+        let _ = std::fs::read_to_string(p).unwrap();
+    }
+
     let mut gen = Generator::new(Style::ItgDoubles, GeneratorParameters::default());
     let _ = gen.gen();
 }
